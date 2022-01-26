270355e71c1c89cb3b29958116b2b43ca5b80f18
lgersman <lars.gersmann@gmail.com>
2022-01-26 11:32:02 +0100

    fix: added missing "Tested up to" metdata to plugin.php and readme.txt template

--- plugins/cm4all-wp-impex-example/plugin.php
+++ plugins/cm4all-wp-impex-example/plugin.php
@@ -8,6 +8,7 @@
  * Tags: import, export, migration
  * Requires PHP: 8.0
  * Requires at least: 5.7
+ * Tested up to: 5.9
  * Author: Lars Gersmann, CM4all
  * Author URI: https://cm4all.com
  * Domain Path: /languages
