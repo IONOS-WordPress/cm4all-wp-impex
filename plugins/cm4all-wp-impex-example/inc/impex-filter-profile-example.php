6356f97b909a525abd0741b6e4a736614e30cd40
lgersman <lars.gersmann@gmail.com>
2022-11-03 14:17:06 +0100

    fix: disabled example filter hiding impex import profiles

--- plugins/cm4all-wp-impex-example/inc/impex-filter-profile-example.php
+++ plugins/cm4all-wp-impex-example/inc/impex-filter-profile-example.php
@@ -10,10 +10,10 @@ if (!defined('ABSPATH')) {
 /**
  * example export profile filter hiding the default base profile
  */
-\add_filter( 
-  'impex_export_filter_profiles', 
-  fn( $profiles ) => array_filter($profiles, fn($profile) => $profile->name !== 'base'),
-);
+// \add_filter( 
+//   'impex_export_filter_profiles', 
+//   fn( $profiles ) => array_filter($profiles, fn($profile) => $profile->name !== 'base'),
+// );
 
 /**
  * example import profile filter hiding the default "all" profile
