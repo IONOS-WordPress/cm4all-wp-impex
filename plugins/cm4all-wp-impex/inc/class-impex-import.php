a752c5e0021e7d88499bf29daa2c9d6ffcdd141b
Lars Gersmann <lars.gersmann@gmail.com>
2024-01-11 20:21:03 +0100

    fix: final fix for menu id reassociation

--- plugins/cm4all-wp-impex/inc/class-impex-import.php
+++ plugins/cm4all-wp-impex/inc/class-impex-import.php
@@ -320,7 +320,7 @@ abstract class ImpexImport extends ImpexPart
 
     // check/fix menus of current theme
     $theme_slug = \get_option('stylesheet');
-    $theme_mods = \get_option('theme_mods_${theme_slug}');
+    $theme_mods = \get_option("theme_mods_${theme_slug}");
     $nav_menu_locations = $theme_mods['nav_menu_locations'] ?: [];
     // https://wordpress.stackexchange.com/questions/124658/setting-a-default-theme-location-when-creating-a-menu
 
