406283e1363a4d5178496b5616fd6ddfda2593e8
Lars Gersmann <lars.gersmann@gmail.com>
2024-01-11 12:18:42 +0100

    fix: upgraded rector config to work with latest rector release

--- rector.php
+++ rector.php
@@ -9,16 +9,29 @@ use Rector\Set\ValueObject\DowngradeSetList;
 use Rector\Set\ValueObject\DowngradeLevelSetList;
 
 return static function (RectorConfig $rectorConfig): void {
-  // get parameters
-  $parameters = $rectorConfig->parameters();
-  $parameters->set(Option::PATHS, [
-    __DIR__ . '/dist/cm4all-wp-impex-php7.4.0'
-  ]);
-  $parameters->set(Option::PARALLEL, false);
+  // // get parameters
+  // $parameters = $rectorConfig->parameters();
+  // $parameters->set(Option::PATHS, [
+  //   __DIR__ . '/dist/cm4all-wp-impex-php7.4.0'
+  // ]);
+  // $parameters->set(Option::PARALLEL, false);
 
-  $parameters->set(Option::PHP_VERSION_FEATURES, PhpVersion::PHP_80);
+  // $parameters->set(Option::PHP_VERSION_FEATURES, PhpVersion::PHP_80);
 
-  // Define what rule sets will be applied
-  $rectorConfig->import(DowngradeLevelSetList::DOWN_TO_PHP_80);
-  $rectorConfig->import(DowngradeSetList::PHP_80);
+  // // Define what rule sets will be applied
+  // $rectorConfig->import(DowngradeLevelSetList::DOWN_TO_PHP_80);
+  // $rectorConfig->import(DowngradeSetList::PHP_80);
+
+  $rectorConfig->paths([__DIR__ . '/dist/cm4all-wp-impex-php7.4.0']);
+
+  $rectorConfig->skip([__DIR__ . '/vendor']);
+  $rectorConfig->disableParallel();
+  $rectorConfig->phpVersion(PhpVersion::PHP_80);
+
+  $rectorConfig->sets(
+    [
+      DowngradeLevelSetList::DOWN_TO_PHP_80,
+      DowngradeSetList::PHP_80
+    ]
+  );
 };
